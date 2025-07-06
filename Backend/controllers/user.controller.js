4dd13a88f46bea7e42dfbf051580d76a0ff1c7d4
Tanishq Sonwane <tanishqsonwane@gmail.com>
2025-07-06 21:52:41 +0530

    redis connected

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,7 +1,7 @@
 import { validationResult } from 'express-validator';
 import * as userService from '../services/user.service.js';
 import  usermodel from '../models/user.model.js';
-
+import redisClient from '../services/redis.service.js';
 export const createUserController = async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
@@ -47,7 +47,6 @@ export const loginUserController = async (req, res) => {
     }
 }
   export const getUserProfileController = async (req, res) => {
-    console.log(req.user)
     res.status(200).json({
       user: req.user,
     });
