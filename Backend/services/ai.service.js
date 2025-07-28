9d730bc1d19a47a375977a47d7095bd30e251914
Tanishq Sonwane <tanishqsonwane32@gmail.com>
2025-07-28 16:50:09 +0530

    instructions added

--- Backend/services/ai.service.js
+++ Backend/services/ai.service.js
@@ -4,8 +4,9 @@ import dotenv from "dotenv";
 dotenv.config(); 
 
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_AI_KEY);
-
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash",
+    systemInstruction:`you are an expert in MERN and Development. you have an experience of 10 years in the development. you always write code in modular and break the code in the possible way and follow best practice, you use understandable comments in the code, you create files as needed. you write code while maintaining the working of previous code. you always follow the best pracitce of the development you never miss the edge cases and always write code that is scalable and maintainance, in your code you always handle the errors and expections.`
+ });
 
 export const generateContent = async (prompt) => {
   try {
