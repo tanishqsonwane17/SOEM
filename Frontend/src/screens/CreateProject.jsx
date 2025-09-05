2e9bc67d59c0ca365168ba39f2957a33a27d11e8
Tanishq Sonwane <tanishqsonwane32@gmail.com>
2025-09-05 16:30:06 +0530

    project page theme changed

--- Frontend/src/screens/CreateProject.jsx
+++ Frontend/src/screens/CreateProject.jsx
@@ -28,17 +28,17 @@ const res = await axios.post(
   }
 
   return (
-    <div className='h-screen w-full flex justify-center items-center bg-slate-100'>
-      <div className='h-[40%] w-[30%] bg-slate-200 rounded-2xl'>
+    <div className='h-screen w-full flex justify-center items-center bg-[#323232]'>
+      <div className='h-[40%] w-[30%] bg-[#414141] rounded-2xl'>
         <div className='h-full w-full '>
-          <h2 className='text-center p-4 text-3xl tracking-tight font-bold'>
-            Create project
+          <h2 className='text-center text-[#e6e6e6] p-4 text-3xl tracking- font-semibold'>
+            Create Project
           </h2>
           <div className='h-40 px-20 gap-4 w-full flex justify-center items-center flex-col'>
             {/* Project Name Input */}
             <input
               type="text"
-              className='border py-2 w-full px-2 rounded-sm'
+              className='border border-[#e6e6e68b] text-white py-2 w-full px-2 rounded-sm'
               placeholder='Project name'
               value={name}
               onChange={(e) => setName(e.target.value)}
@@ -47,8 +47,7 @@ const res = await axios.post(
             {/* Submit Button */}
             <button
               onClick={handleCreateProject}
-              className='border bg-black text-white py-2 rounded-xl cursor-pointer w-full px-2 '
-            >
+              className=' border border-[#e6e6e68b] bg-black text-white py-2 rounded-xl cursor-pointer w-full px-2 '>
               Create Project
             </button>
           </div>
